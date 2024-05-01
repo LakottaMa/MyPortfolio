ba64e7f909f0c4f6075d4664ed6aec6a79fee7ad
Marcel Lakotta <marcel.lakotta@aol.de>
2024-05-01 20:20:46 +0200

    first setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./shared/header/header.component";
+import { FooterComponent } from "./shared/footer/footer.component";
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, CommonModule, HeaderComponent, FooterComponent, LandingPageComponent]
 })
 export class AppComponent {
-  title = 'MyPortfolio';
+  title = 'Marcel Lakotta Portfolio';
 }
