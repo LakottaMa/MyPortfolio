448d782e39de83427d0754f3149ed1c991a48127
Marcel Lakotta <marcel.lakotta@aol.de>
2024-05-03 09:51:25 +0200

    add components, images & style profil component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, HostListener} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./shared/header/header.component";
-import { FooterComponent } from "./shared/footer/footer.component";
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [RouterOutlet, CommonModule, HeaderComponent, FooterComponent, LandingPageComponent]
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, LandingPageComponent, HeaderComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Marcel Lakotta Portfolio';
+
+  private scrollTimeout: any;
+
+  @HostListener('window:scroll')
+  onScroll() {
+    const scrollbarThumbColor = '--scrollbar-thumb-color';
+    const defaultScrollbarThumbColor = '';
+    document.documentElement.style.setProperty(scrollbarThumbColor, '#00bee8');
+    clearTimeout(this.scrollTimeout);
+    this.scrollTimeout = setTimeout(() => {
+      document.documentElement.style.setProperty(scrollbarThumbColor, defaultScrollbarThumbColor);
+    }, 50);
+  }
+  ngOnDestroy() {
+    clearTimeout(this.scrollTimeout);
+  }
 }
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -2,13 +2,18 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../shared/header/header.component";
 import { FooterComponent } from "../shared/footer/footer.component";
 import { CommonModule } from '@angular/common';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProfilComponent } from "./profil/profil.component";
 
 @Component({
     selector: 'app-landing-page',
     standalone: true,
     templateUrl: './landing-page.component.html',
     styleUrl: './landing-page.component.scss',
-    imports: [HeaderComponent, FooterComponent, CommonModule]
+    imports: [HeaderComponent, FooterComponent, CommonModule, PortfolioComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, AboutMeComponent, SkillsComponent, ContactComponent, ProfilComponent]
 })
 export class LandingPageComponent {
 
