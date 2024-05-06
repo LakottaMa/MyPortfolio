26e18e9b05ab2bd68667d658db51516be0a27073
Marcel Lakotta <marcel.lakotta@aol.de>
2024-05-06 20:22:44 +0200

    add portfolio components

---
+++ src/app/shared/portfolio-template1/portfolio-template1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-template1',
+  standalone: true,
+  imports: [],
+  templateUrl: './portfolio-template1.component.html',
+  styleUrl: './portfolio-template1.component.scss'
+})
+export class PortfolioTemplate1Component {
+
+}
---
+++ src/app/shared/portfolio-template2/portfolio-template2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-template2',
+  standalone: true,
+  imports: [],
+  templateUrl: './portfolio-template2.component.html',
+  styleUrl: './portfolio-template2.component.scss'
+})
+export class PortfolioTemplate2Component {
+
+}
