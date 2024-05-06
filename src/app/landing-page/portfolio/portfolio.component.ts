87364c2514737446cda780f182ef2950c808fb9c
Marcel Lakotta <marcel.lakotta@aol.de>
2024-05-06 20:23:40 +0200

    edit portfolio components

--- src/app/landing-page/portfolio/portfolio.component.ts
+++ src/app/landing-page/portfolio/portfolio.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { PortfolioTemplate2Component } from '../../shared/portfolio-template2/portfolio-template2.component';
+import { PortfolioTemplate1Component } from '../../shared/portfolio-template1/portfolio-template1.component';
 
 @Component({
-  selector: 'app-portfolio',
-  standalone: true,
-  imports: [],
-  templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+    selector: 'app-portfolio',
+    standalone: true,
+    templateUrl: './portfolio.component.html',
+    styleUrl: './portfolio.component.scss',
+    imports: [PortfolioTemplate2Component, PortfolioTemplate1Component]
 })
 export class PortfolioComponent {
+
 
 }
