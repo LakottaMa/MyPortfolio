c69b15de86946b19111b02e4d6dd132f74aba852
Marcel Lakotta <marcel.lakotta@aol.de>
2024-05-11 11:35:42 +0200

    add burger menu, edit responsive design

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -2,16 +2,25 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ServicesComponent } from '../services/services.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 
 export class HeaderComponent {
+  menuValue: boolean = false;
+  openMenu() {
+    this.servicesComponent.isActiveLink = '';
+  }
+  closeMenu() {
+    this.menuValue = false;
+  }
+
   constructor(private servicesComponent: ServicesComponent) { }
   setActiveLink(link: string): void {
     this.servicesComponent.setActiveLink(link);
