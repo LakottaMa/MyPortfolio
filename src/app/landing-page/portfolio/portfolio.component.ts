1855cb8d6f994f05e93cb3b47da9dead61df6eed
Marcel Lakotta <marcel.lakotta@aol.de>
2024-07-11 10:34:41 +0200

    add new project

--- src/app/landing-page/portfolio/portfolio.component.ts
+++ src/app/landing-page/portfolio/portfolio.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { PortfolioTemplate2Component } from '../../shared/portfolio-template2/portfolio-template2.component';
 import { PortfolioTemplate1Component } from '../../shared/portfolio-template1/portfolio-template1.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { PortfolioTemplate0Component } from "../../shared/portfolio-template0/portfolio-template0.component";
 @Component({
     selector: 'app-portfolio',
     standalone: true,
     templateUrl: './portfolio.component.html',
     styleUrl: './portfolio.component.scss',
-    imports: [PortfolioTemplate2Component, PortfolioTemplate1Component, TranslateModule]
+    imports: [PortfolioTemplate2Component, PortfolioTemplate1Component, TranslateModule, PortfolioTemplate0Component]
 })
 export class PortfolioComponent {
 }
---
+++ src/app/shared/portfolio-template0/portfolio-template0.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-portfolio-template0',
+  standalone: true,
+  imports: [TranslateModule],
+  templateUrl: './portfolio-template0.component.html',
+  styleUrl: './portfolio-template0.component.scss'
+})
+export class PortfolioTemplate0Component {
+
+}
